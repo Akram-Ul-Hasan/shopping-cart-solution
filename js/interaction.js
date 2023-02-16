2c591f07f941ac9bf1f25e05f23d9ee2bc00b1fe
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-02-16 20:43:55 +0600

    complete

--- js/interaction.js
+++ js/interaction.js
@@ -24,21 +24,41 @@ function elementPriceUsingProductNumber(elementId,productNumber,price){
     totalElementPriceAmount.innerText=totalPrice;
 } 
 
+function subtotalTaxTotalPrice(){
+    const subtotalAmountField = document.getElementById('subtotal-amount-field');
+    const taxAmountField = document.getElementById('tax-amount-field');
+    const totalAmountField = document.getElementById('total-amount-field');
+    const totalCasePriceAmount =document.getElementById('total-case-price-amount');
+    const totalPhonePriceAmount =document.getElementById('total-phone-price-amount');
+
+    subtotalAmountField.innerText = parseFloat(totalCasePriceAmount.innerText)+parseFloat(totalPhonePriceAmount.innerText);
+
+    taxAmountField.innerText = (parseFloat(subtotalAmountField.innerText)*.1).toFixed(2);
+
+    totalAmountField.innerText = (parseFloat(subtotalAmountField.innerText)+parseFloat(taxAmountField.innerText)).toFixed(2);
+}
 document.getElementById("btn-case-plus").addEventListener("click", function () {
     const caseBuyCount = cartElementAdjustment('case-number-field',true);
-    elementPriceUsingProductNumber('total-case-price-amount',caseBuyCount,59)
+    elementPriceUsingProductNumber('total-case-price-amount',caseBuyCount,59);
+    subtotalTaxTotalPrice();
 });
 document.getElementById("btn-case-minus").addEventListener("click", function () {
     const caseBuyCount = cartElementAdjustment('case-number-field',false);
-    elementPriceUsingProductNumber('total-case-price-amount',caseBuyCount,59)
+    elementPriceUsingProductNumber('total-case-price-amount',caseBuyCount,59);
+    subtotalTaxTotalPrice();
+
 });
 
 document.getElementById("btn-phone-plus").addEventListener("click", function () {
     const phoneBuyCount = cartElementAdjustment('phone-number-field', true);
-    elementPriceUsingProductNumber('total-phone-price-amount',phoneBuyCount,1219)
+    elementPriceUsingProductNumber('total-phone-price-amount',phoneBuyCount,1219);
+    subtotalTaxTotalPrice();
+
 });
 document.getElementById("btn-phone-minus").addEventListener("click", function () {
     const phoneBuyCount = cartElementAdjustment('phone-number-field',false);
-    elementPriceUsingProductNumber('total-phone-price-amount',phoneBuyCount,1219)
+    elementPriceUsingProductNumber('total-phone-price-amount',phoneBuyCount,1219);
+    subtotalTaxTotalPrice();
+
 });
 
